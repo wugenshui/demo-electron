3cca5f5040963c280eb48a2b46d9b3276890ed3d
chenbo <963838470@qq.com>
2021-05-15 14:27:22 +0800

    增加cmd命令行工具

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@
 
 <script>
 const { require } = window
+const exec = require('child_process').exec
 const { ipcRenderer } = require('electron')
 
 export default {
@@ -21,6 +22,20 @@ export default {
   },
   mounted() {
     console.log(process.env)
+    // 任何你期望执行的cmd命令，ls都可以
+    let workerProcess = exec('ipconfig')
+    // 打印正常的后台可执行程序输出
+    workerProcess.stdout.on('data', function(data) {
+      console.log('stdout: ' + data)
+    })
+    // 打印错误的后台可执行程序输出
+    workerProcess.stderr.on('data', function(data) {
+      console.log('stderr: ' + data)
+    })
+    // 退出之后的输出
+    workerProcess.on('close', function(code) {
+      console.log('out code：' + code)
+    })
   },
   methods: {
     auto() {
